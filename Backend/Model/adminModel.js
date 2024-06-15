4e0137afe65047b4c174b0e1c33564ecd01393f6
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-06-16 02:25:30 +0300

    The admin model has been created.

---
+++ Backend/Model/adminModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const adminModel = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      require: true,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    createdApplications: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Application",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+adminModel.pre("save", async function (next) {
+  const hashedPassword = await bcrypt.genSalt(10);
+  this.password = hashedPassword;
+});
