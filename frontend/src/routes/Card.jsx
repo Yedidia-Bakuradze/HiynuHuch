eecf894771688ffc072e6273ab3395a6aa7ad1af
Meir Crombie <meircrombie@gmail.com>
2024-06-23 15:45:24 +0300

    finish with the card commponent and with the style of it - meir

---
+++ frontend/src/routes/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+  return (
+    <div className="card">
+        
+      <p className="card_text">Name:</p>
+      <p className="card_text">Status:</p>
+      <p className="card_text">More parametrs:</p>
+        
+        <button className="view_button ">Open</button>
+    </div>
+  );
+}
+    
+export default Card;
