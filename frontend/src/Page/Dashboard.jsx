5c5831d6bbb54a8a2514bb9b21c90e4dd01a9c80
Meir Crombie <meircrombie@gmail.com>
2024-06-24 22:11:58 +0300

    Did test on the dashboard page and its worked. finish woth the dashboard page without designing.

--- frontend/src/Page/Dashboard.jsx
+++ frontend/src/Page/Dashboard.jsx
@@ -1,19 +1,28 @@
-import { Card,Button } from "react-bootstrap";
-import { useState, useEffect } from "react";
+import { Card,Button,Row,Col } from "react-bootstrap";
 import './Dashboard.css';
+import { emps } from "../data/emps.js";
 function Dashboard() {
   return (
-    <Card className="Card" style={{ width: '15rem', height: '20rem' }}>
-      <Card.Header>Header</Card.Header>
-      <Card.Body>
-      <Card.Text><h5>Name:</h5></Card.Text>
-      <Card.Text><h5>Age:</h5></Card.Text>
-      <Card.Text><h5>Email:</h5></Card.Text>
-      <Card.Text><h5>Grade:</h5></Card.Text>
-      <Card.Text><h5>Status:</h5></Card.Text>
-      <Button  variant="primary">More Details</Button>
-      </Card.Body>
-    </Card>
+    <>
+    
+    <Row className="CardRow"> 
+      {emps.map((emp) => (
+        
+        <Card className="Card" key={emp.id}>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Text><h6>Name: {emp.name}</h6></Card.Text>
+            <Card.Text><h6>Email: {emp.email}</h6></Card.Text>
+            <Card.Text><h6>Grade: {emp.grade}</h6></Card.Text>
+            <Card.Text><h6>Status: {emp.status}</h6></Card.Text>
+            <Button variant="primary">More Details</Button>
+          </Card.Body>
+        </Card>
+
+      ))}
+    </Row>
+    </>
+
   );  
 }
 
--- frontend/src/Page/appllyForm.jsx
+++ frontend/src/Page/appllyForm.jsx
@@ -1,46 +1,37 @@
-import { Form, Button, } from "react-bootstrap";
-import {emps} from "../data/emps.js";
-import {positions} from "../data/positions.js";
+import { Form, Button } from "react-bootstrap";
+import { emps } from "../data/emps.js";
+import { positions } from "../data/positions.js";
 
 
 function ApplyForm() {
   return (
-    <ul>
-      {emps.map((user) => (
-        <li key={user._id && user._id.$oid ? user._id.$oid : user.name}>
-          {user.name}
-        </li>
-      ))}
-            {positions.map((user) => (
-        <li key={user._id && user._id.$oid ? user._id.$oid : user.name}>
-          {user.title}
-        </li>
-      ))}
-    </ul>
-
-    
+    <div className="d-flex justify-content-center align-items-center min-vh-100">
+      <div className="apply-form-container border rounded p-4">
+        <Form method="post">
+          <Form.Group>
+            <Form.Label>
+              <h1>Apply form for a [job title]</h1>
+            </Form.Label>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label><h3>Your name:</h3></Form.Label>
+            <Form.Control type="Name" placeholder="Enter your name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label><h3>Email address</h3></Form.Label>
+            <Form.Control type="Email" placeholder="Email address" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicResume">
+            <Form.Label><h3>Please upload your Cv:</h3></Form.Label>
+            <Form.Control type="file" placeholder="Your File:" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 }
+
 export default ApplyForm;
-
-
-      {/* <Form method="post">
-        <Form.Group>
-          <Form.Label><h1>Apply form for a [job title]</h1></Form.Label>
-        </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label><h3>Your name:</h3></Form.Label>
-        <Form.Control type="Name" placeholder="Enter your name" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label><h3>Email address</h3></Form.Label>
-        <Form.Control type="Email" placeholder="Email address" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicResume">
-        <Form.Label><h3>Please upload your Cv:</h3></Form.Label>
-        <Form.Control type="file" placeholder="Your File:" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form> */}
