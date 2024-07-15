0afcf62cad4adc2bfd9452a6f411db5098d116de
Meir Crombie <meircrombie@gmail.com>
2024-07-15 22:12:02 +0300

    Start to design the filter popup windowe

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -4,7 +4,6 @@ import { emps } from "../data/emps.js";
 function Dashboard() {
   return (
     <>
-<<<<<<< HEAD
     <Row className="CardRow"> 
       {emps.map((emp) => (
         <Card className="Card" key={emp.id}>
@@ -22,33 +21,6 @@ function Dashboard() {
       ))}
     </Row>
       
-=======
-      <Row className="CardRow">
-        {emps.map((emp) => (
-          <Card className="Card" key={emp.id}>
-            <Card.Header>Header</Card.Header>
-            <Card.Body>
-              <Card.Text>
-                <h8>Name: {emp.name}</h8>
-              </Card.Text>
-              <Card.Text>
-                <h8>Email: {emp.email}</h8>
-              </Card.Text>
-              <Card.Text>
-                <h8>Grade: {emp.grade}</h8>
-              </Card.Text>
-              <Card.Text>
-                <h8>Status: {emp.status}</h8>
-              </Card.Text>
-              <ProgressBar now={10} label={`${10}%`} />
-              <Button className="Submit-btn" variant="primary">
-                More Details
-              </Button>
-            </Card.Body>
-          </Card>
-        ))}
-      </Row>
->>>>>>> 2c70ff612c15e984d74f7b9718359565afd29b03
     </>
   );
 }
