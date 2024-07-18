402937f5bd2f3cb7834865d6068e195131e577ac
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-07-18 11:11:01 +0300

    The login function logic has been started to implemented.

--- frontend/src/Screens/LoginScreen.jsx
+++ frontend/src/Screens/LoginScreen.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
 import "../Style/login.css";
+import { useState } from "react";
 import { redirect, useNavigate, Link } from "react-router-dom";
 
 export default function LoginScreen() {
   const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  //Verifies if the user exists in the database
+  const verifyUserLogin = (e)=>{
+    //Fetch data from backend and see if user exists
+    alert(username)
+    alert(password)
+
+    //If hes not - Error message
+    
+    //If he is - navigate to /:id with the id provided by the backend
+
+
+  }
+
+
   return (
     <div className="center-div">
       <Form className="border rounded ">
@@ -15,6 +33,7 @@ export default function LoginScreen() {
           placeholder="exmaple@gmail.com"
           size="lg"
           name="email"
+          onChange={(e)=>setUsername(e.target.value)}
         />
 
         <Form.Label className="fieldsnames">Password:</Form.Label>
@@ -24,13 +43,15 @@ export default function LoginScreen() {
           placeholder="********"
           size="lg"
           name="password"
+          onChange={(e)=>{setPassword(e.value.target)}}
         />
+
         <p id="notloginbuttons">
           <Link to={`/forgotpassword`} className="smallbuttons">
             Forgot password?
           </Link>
 
-          <Link to={`/signup`} className="smallbuttons">
+          <Link to={`/recruiter/signup`} className="smallbuttons">
             signup
           </Link>
         </p>
@@ -47,9 +68,7 @@ export default function LoginScreen() {
           <button
             type="button"
             className="app_container loginpagebuttons"
-            onClick={() => {
-              navigate("/");
-            }}
+            onClick={(e)=>verifyUserLogin(e)}
           >
             Cancel
           </button>
